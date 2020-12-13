6f78b2fe2b1a8884bb53e1cc9f735363499bb539
Xiaoli Wei <xiaoliwei@WSBMBP15.local>
2020-12-13 17:30:29 +0800

    test projects

---
+++ Threshold/target/debug/deps/Threshold-06f9ae3a8f94f637.d
@@ -0,0 +1,5 @@
+/Users/xiaoliwei/DevOps/shibowe/RustX/Threshold/target/debug/deps/Threshold-06f9ae3a8f94f637.rmeta: src/main.rs
+
+/Users/xiaoliwei/DevOps/shibowe/RustX/Threshold/target/debug/deps/Threshold-06f9ae3a8f94f637.d: src/main.rs
+
+src/main.rs:
---
+++ Threshold/target/debug/deps/Threshold-7e25d0e9ac52f497.d
@@ -0,0 +1,5 @@
+/Users/xiaoliwei/DevOps/shibowe/RustX/Threshold/target/debug/deps/Threshold-7e25d0e9ac52f497.rmeta: src/main.rs
+
+/Users/xiaoliwei/DevOps/shibowe/RustX/Threshold/target/debug/deps/Threshold-7e25d0e9ac52f497.d: src/main.rs
+
+src/main.rs:
