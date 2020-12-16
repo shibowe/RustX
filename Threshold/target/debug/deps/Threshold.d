702367c0c78b414e4fe3c8aa5c01ab5d6f9bf9ab
Xiaoli Wei <dev@winecos.com>
2020-12-16 15:26:31 +0800

    re-compile the project

---
+++ Threshold/target/debug/deps/Threshold.d
@@ -0,0 +1,5 @@
+/Users/xiaoliwei/DevOps/shibowe/RustX/Threshold/target/debug/deps/Threshold: src/main.rs
+
+/Users/xiaoliwei/DevOps/shibowe/RustX/Threshold/target/debug/deps/Threshold.d: src/main.rs
+
+src/main.rs:
