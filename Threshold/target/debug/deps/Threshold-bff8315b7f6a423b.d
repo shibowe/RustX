fa54d32b6a2c18e8397c4b0119d0200915cf89d3
Xiaoli Wei <dev@winecos.com>
2020-12-16 15:28:17 +0800

    delete all the target folder

--- Threshold/target/debug/deps/Threshold-bff8315b7f6a423b.d
+++
@@ -1,5 +0,0 @@
-/Users/xiaoliwei/DevOps/shibowe/RustX/Threshold/target/debug/deps/Threshold-bff8315b7f6a423b: src/main.rs
-
-/Users/xiaoliwei/DevOps/shibowe/RustX/Threshold/target/debug/deps/Threshold-bff8315b7f6a423b.d: src/main.rs
-
-src/main.rs:
